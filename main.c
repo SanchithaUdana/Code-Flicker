40301f333873d4264ee12a467032df289ffe9b73
Sanchitha Udana Wijesundara <109952575+SanchithaUdana@users.noreply.github.com>
2024-05-27 20:42:37 +0530

    library inport and defining and structures

--- main.c
+++ main.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX_STACK_SIZE 100
+
+typedef struct {
+    double stack[MAX_STACK_SIZE];
+    int top;
+} Stack;
+
+
+
 
 int main() {
     printf("Hello, World!\n");
